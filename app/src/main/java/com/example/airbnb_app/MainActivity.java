33fc95576e173194cc9256ec08b2517d9c0275a6
thanosd13 <adimopoulos@ceid.upatras.gr>
2024-05-14 20:07:53 +0300

    new v

--- app/src/main/java/com/example/airbnb_app/MainActivity.java
+++ app/src/main/java/com/example/airbnb_app/MainActivity.java
@@ -2,22 +2,12 @@ package com.example.airbnb_app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.airbnb_app.adapter.RecentsAdapter;
-import com.example.airbnb_app.adapter.TopPlacesAdapter;
 import com.example.airbnb_app.databinding.ActivityMainBinding;
-import com.example.airbnb_app.model.RecentsData;
-import com.example.airbnb_app.model.TopPlacesData;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
